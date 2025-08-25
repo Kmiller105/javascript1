d53b5a21916e74634531e9377d257f349beff85a
Kimberly miller <135668982+Kmiller105@users.noreply.github.com>
2025-08-25 13:58:49 -0500

    update

--- java.js
+++ java.js
@@ -1,2 +1,10 @@
-console.log("Is This Right?");
-document.write("Can You See This?")
+
+var life=100;
+var name ="Kim";
+life = life -10;
+life = life -10;
+var globalvar1= "Hi";
+var globalvar2="World";
+let blockscopedlet ="Im Trapped in a box.";
+console.log(globalvar1,globalvar2);
+
