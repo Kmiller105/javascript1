da06d447e62db7728233c9ac5c623664068c8f6f
Kimberly miller <135668982+Kmiller105@users.noreply.github.com>
2025-08-25 14:08:47 -0500

    up

--- java.js
+++ java.js
@@ -6,5 +6,8 @@ life = life -10;
 var globalvar1= "Hi";
 var globalvar2="World";
 let blockscopedlet ="Im Trapped in a box.";
+let item=25;
+const cars="BMW, Volvo";
+
 console.log(globalvar1,globalvar2);
 
